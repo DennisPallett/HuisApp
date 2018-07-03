50ddf05dc9c1365a5e24e79ff2a196abb213c6c3
Dennis Pallett <dennis.pallett@topicus.nl>
2018-07-03 22:30:43 +0200

    Kleine herstructering api

--- financieel/api/public/index.php
+++ financieel/api/public/index.php
@@ -1,38 +1,21 @@
 <?php
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
 
-require '../vendor/autoload.php';
-require '../config.php';
+// locate file that contains config path
+$configPathFile = dirname(__FILE__) . '/config.path';
+if (!file_exists($configPathFile))
+{
+	die("ERROR: unable to find config path file: " . $configPathFile);
+}
 
-$app = new \Slim\App(['settings' => $config]);
+$configFile = file_get_contents($configPathFile);
 
-$container = $app->getContainer();
+if (!file_exists($configFile)) {
+	die("ERROR: unable to read config file: " . $configFile);
+}
 
-$container['db'] = function ($c) {
-	$db = $c['settings']['db'];
-    $pdo = new PDO($db['connectionString'], $db['user'], $db['password']);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    return $pdo;
-};
+// load config file
+require $configFile;
 
-$app->get('/transacties', TransactiesAction::class);
-
-$app->get('/statements', StatementsAction::class);
-$app->post('/statements/delete', StatementsAction::class . ':delete');
-$app->post('/statements/import', StatementsAction::class . ':import');
-
-$app->post('/transacties/classify', TransactiesAction::class . ':classify');
-$app->post('/transacties/update-category', TransactiesAction::class . ':updateCategory');
-
-$app->get('/categories', CategoriesAction::class);
-$app->get('/categories/groups', CategoriesAction::class . ':groups');
-
-$app->get('/dates/months', DatesAction::class  . ':months');
-
-$app->get('/reporting/category-by-month', ReportingAction::class  . ':categoryByMonth');
-$app->get('/reporting/balance', ReportingAction::class  . ':balance');
-
-
-$app->run();
+// run app
+$configPath = dirname($configFile);
+require $configPath . '/run.php';
---
+++ financieel/api/run.php
@@ -0,0 +1,38 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+$basePath = dirname(__FILE__);
+
+require $basePath . '/vendor/autoload.php';
+
+$app = new \Slim\App(['settings' => $config]);
+
+$container = $app->getContainer();
+
+$container['db'] = function ($c) {
+	$db = $c['settings']['db'];
+    $pdo = new PDO($db['connectionString'], $db['user'], $db['password']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $pdo;
+};
+
+$app->get('/transacties', TransactiesAction::class);
+
+$app->get('/statements', StatementsAction::class);
+$app->post('/statements/delete', StatementsAction::class . ':delete');
+$app->post('/statements/import', StatementsAction::class . ':import');
+
+$app->post('/transacties/classify', TransactiesAction::class . ':classify');
+$app->post('/transacties/update-category', TransactiesAction::class . ':updateCategory');
+
+$app->get('/categories', CategoriesAction::class);
+$app->get('/categories/groups', CategoriesAction::class . ':groups');
+
+$app->get('/dates/months', DatesAction::class  . ':months');
+
+$app->get('/reporting/category-by-month', ReportingAction::class  . ':categoryByMonth');
+$app->get('/reporting/balance', ReportingAction::class  . ':balance');
+
+$app->run();
