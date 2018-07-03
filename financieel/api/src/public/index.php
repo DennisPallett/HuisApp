21bd1c66842b9cb3d11bba383326b5da5562ee53
Dennis Pallett <dennis.pallett@topicus.nl>
2018-07-03 21:34:24 +0200

    Connectie string in config specifieren

--- financieel/api/src/public/index.php
+++ financieel/api/src/public/index.php
@@ -10,16 +10,7 @@ $app = new \Slim\App(['settings' => $config]);
 $container = $app->getContainer();
 
 $container['db'] = function ($c) {
-    $db = $c['settings']['db'];
-
-	$connStr = sprintf("pgsql:host=%s;port=%d;dbname=%s;user=%s;password=%s", 
-    $db['host'], 
-    $db['port'], 
-    $db['database'], 
-    $db['user'], 
-    $db['password']);
-
-    $pdo = new PDO($connStr);
+    $pdo = new PDO($c['settings']['databaseConnectionString']);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     return $pdo;
